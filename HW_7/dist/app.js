b9925335fbf46aefb9883c2133b4927828cd4572
Oksana Plotnikova <oksankapl17@gmail.com>
2019-10-27 21:25:22 +0200

    HW7

--- HW_7/dist/app.js
+++ HW_7/dist/app.js
@@ -4,21 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const inquirer_1 = __importDefault(require("inquirer"));
-const questions = [{
-        type: 'rawlist',
-        name: 'selection',
-        message: "Please choose an option:",
-        choices: ["Верховна рада", "Фракція", "Депутат"],
-        validation: (answer) => {
-            if (!["Верховна рада", "Фракція", "Депутат"].includes(answer)) {
-                return 'Please choose valid option';
-            }
-            return true;
-        }
-    }];
+const questions_1 = require("./questions");
 async function start() {
     try {
-        const { selection } = await inquirer_1.default.prompt(questions);
+        const { selection } = await inquirer_1.default.prompt(questions_1.generalQuestions);
         switch (selection) {
             case "Верховна рада":
                 console.log(1);
